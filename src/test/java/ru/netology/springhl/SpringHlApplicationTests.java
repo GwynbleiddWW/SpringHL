322b6e27e484f03e4ef91926bf8464621d42cd5f
Konstantin Shebalin <championacolironni@gmail.com>
2021-08-28 00:29:28 +0700

    Dock

--- src/test/java/ru/netology/springhl/SpringHlApplicationTests.java
+++ src/test/java/ru/netology/springhl/SpringHlApplicationTests.java
@@ -1,13 +1,36 @@
 package ru.netology.springhl;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+import org.testcontainers.containers.GenericContainer;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SpringHlApplicationTests {
+
+    @Autowired
+    TestRestTemplate restTemplate;
+    public static GenericContainer<?> devapp = new GenericContainer<>("devapp:latest")
+            .withExposedPorts(8090);
+    public static GenericContainer<?> prodapp = new GenericContainer<>("progapp:latest")
+            .withExposedPorts(8081);
+
+
+    @BeforeAll
+    public static void setUp() {
+        devapp.start();
+        prodapp.start();
+    }
 
     @Test
     void contextLoads() {
-    }
+        ResponseEntity<String> forEntity = restTemplate.getForEntity("http://localhost:" + devapp.getMappedPort(8080), String.class);
+        System.out.println(forEntity.getBody());
 
+        ResponseEntity<String> forEntityTwo = restTemplate.getForEntity("http://localhost:" + prodapp.getMappedPort(8080), String.class);
+        System.out.println(forEntityTwo.getBody());
+    }
 }
